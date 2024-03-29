6ad2750150dfb1707a0a836042135c3b0a88aca0
santiagofv36 <59099144+santiagofv36@users.noreply.github.com>
2024-03-29 13:26:14 -0400

    feat(client): Pages Setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,30 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {
+  CartPage,
+  CheckoutPage,
+  HomePage,
+  LoginPage,
+  NotFoundPage,
+  ProductDetailPage,
+  ProductPage,
+  SignupPage,
+} from './pages';
+
 function App() {
   return (
     <>
-      <h1 className="text-rose-600">App</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/products" element={<ProductPage />} />
+          <Route path="/products/:id" element={<ProductDetailPage />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/checkout" element={<CheckoutPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,7 @@
+// interface HomePageProps {}
+
+const HomePage = () => {
+  return <div>Home</div>;
+};
+
+export default HomePage;
