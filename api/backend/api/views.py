c9581cb1536ddd86211ed3492aced10634c37346
santiagofv36 <59099144+santiagofv36@users.noreply.github.com>
2024-03-29 02:26:20 -0400

    feat(api): Creat Product endpoint

--- api/backend/api/views.py
+++ api/backend/api/views.py
@@ -1,12 +1,30 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from .models import Product
 from rest_framework import generics
-from .serializers import UserSerializer
+from .serializers import ProductSerializer, UserSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
 
 
-# Create your views here.
+class ProductListCreate(generics.ListCreateAPIView):
+    serializer_class = ProductSerializer
+    permission_classes = [
+        AllowAny,
+    ]
+
+    def get_queryset(self):
+        return Product.objects.all()
+
+    def perform_create(self, serializer):
+        if serializer.is_valid():
+            serializer.save()
+        else:
+            print(serializer.errors)
+
+
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [AllowAny,]
+    permission_classes = [
+        AllowAny,
+    ]
