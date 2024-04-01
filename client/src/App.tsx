f08ba4603fdec02abc6361c89295271121166f85
santiagofv36 <59099144+santiagofv36@users.noreply.github.com>
2024-04-01 16:56:36 -0400

    refactor(client): Use of a routes.tsx file
    - Adaptation with createBrowserRouter
    - Refactor of App component to look much cleaner

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,14 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import {
-  CartPage,
-  CheckoutPage,
-  HomePage,
-  LoginPage,
-  NotFoundPage,
-  ProductDetailPage,
-  ProductPage,
-  SignupPage,
-} from './pages';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import { RouterProvider } from 'react-router-dom';
+import { BrowserRouter } from './routes.tsx';
+import Footer from './components/Footer.tsx';
+import Navbar from './components/Navbar.tsx';
 
 function App() {
   return (
     <main className="w-full max-h-full bg-primary">
-      <BrowserRouter>
-        {/* <Navbar /> */}
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/products" element={<ProductPage />} />
-          <Route path="/products/:id" element={<ProductDetailPage />} />
-          <Route path="/cart" element={<CartPage />} />
-          <Route path="/checkout" element={<CheckoutPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignupPage />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+      <Navbar />
+      <RouterProvider router={BrowserRouter} />
+      <Footer />
     </main>
   );
 }
