9f9842817750950dfcfe9243bcc69326febdbfe0
santiagofv36 <59099144+santiagofv36@users.noreply.github.com>
2024-04-07 23:32:59 -0400

    fix(client): Filter card improvements

--- client/src/components/FilterCard.tsx
+++ client/src/components/FilterCard.tsx
@@ -10,13 +10,19 @@ export default function FilterCard({
   options,
 }: FilterCardProps): JSX.Element {
   return (
-    <div className="flex flex-col gap-4">
-      <h1 className="text-sm font-bold">{title}</h1>
+    <div className="flex flex-col gap-4 border border-border-color p-4 rounded-md shadow-md w-full">
+      <h1 className="text-base font-regular">
+        {' '}
+        <span className="font-bold text-xl">|</span> {title}
+      </h1>
       <ul className="flex flex-col gap-2">
-        {options.map((option) => (
-          <li key={option} className="text-sm">
-            {option}
-          </li>
+        {options.map((option, idx) => (
+          <div className="flex gap-8" key={idx}>
+            <input type="checkbox" />
+            <li key={option} className="text-sm font-thin">
+              {option}
+            </li>
+          </div>
         ))}
       </ul>
     </div>
