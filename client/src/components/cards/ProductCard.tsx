4ac0b4a2826bb9aa60df2a83e9a15eaaeadef772
santiagofv36 <59099144+santiagofv36@users.noreply.github.com>
2024-04-13 00:51:41 -0400

    fix(client): 🔥Using Links in product card

--- client/src/components/cards/ProductCard.tsx
+++ client/src/components/cards/ProductCard.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 interface ProductCardProps {
   product: {
     id: number;
@@ -11,15 +13,17 @@ interface ProductCardProps {
 export default function ProductCard({ product, className }: ProductCardProps) {
   return (
     <div className={`flex flex-col gap-2 ${className}`}>
+      <Link to={`/products/${product?.id}`}>
       <img
         src={product?.image}
         alt={product?.name}
         className="w-full lg:w-80 lg:h-96 rounded-xl"
       />
-      <p className="font-bold">{product?.name}</p>
+      <p className="font-bold mt-2">{product?.name}</p>
+      </Link>
       <div className="flex justify-between items-center">
         <span className="text-sm">${product?.price}</span>
-        <button className="pr-12">+</button>
+        <button>+</button>
       </div>
     </div>
   );
