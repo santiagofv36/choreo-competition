f77c2cf0c1482c1fa8d7958cae0c824a20f6e434
Diogenes <diogenes1991@gmail.com>
2024-04-14 11:50:45 -0400

    Feat: CartItem Controller

---
+++ api/repositories/cartitem_repository.py
@@ -0,0 +1,63 @@
+from dependencies import get_db
+from models.models import CartItem
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+from fastapi import HTTPException,status
+from dtos.cartitem import CreateCartItemRequest
+from uuid import UUID
+from utils import PaginatedResponse
+
+class CartItemRepository:
+        
+        pagesize = 50
+
+        async def create_cartitem(
+                self,
+                db: Session,
+                item : CreateCartItemRequest
+        ):
+                try:
+                        created_cartitem = CartItem(
+                                product_id = UUID(item.product_id),
+                                shopping_cart_id = UUID(item.shopping_cart_id),
+                                quantity = item.quantity
+                                )
+               
+                        db.add(created_cartitem)
+                        db.commit()
+                        db.refresh(created_cartitem)
+                        return created_cartitem
+                except HTTPException as error:
+                        print(error)
+                        return
+
+        async def delete_cartitem(
+                        self,
+                        db : Session,
+                        id : str
+        ):
+                try:
+                        product_to_delete = db.query(CartItem).get(id)
+                        db.delete(product_to_delete)
+                        db.commit()
+                        return
+                except HTTPException as error:
+                        print(error)
+                        return
+        async def get_cartitems(
+                self,
+                db : Session,
+                page : int   
+        ):
+                try:
+                        response = PaginatedResponse(
+                                query = db.query(CartItem),
+                                pagesize = self.pagesize
+                        )
+                        paginated_cartitems = response.get_paginated_results(page=page)
+                        return paginated_cartitems
+                except HTTPException as error:
+                        print(error)
+                        return
+                
+            
