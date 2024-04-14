47d2aac1c7e0611c138b82417110293f28c4a034
santiagofv36 <59099144+santiagofv36@users.noreply.github.com>
2024-04-14 16:04:41 -0400

    fix(client): Bigger text in signup form

--- client/src/components/forms/SignupForm.tsx
+++ client/src/components/forms/SignupForm.tsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import Input from '../inputs/Input';
 import React from 'react';
+import Button from '../inputs/Button';
 
 export default function SignupForm() {
   const [form, setForm] = React.useState({
@@ -23,17 +24,20 @@ export default function SignupForm() {
   return (
     <form className="flex flex-col p-8 w-full" onSubmit={onSubmit}>
       <header className="flex flex-col gap-4">
-        <h1 className="text-4xl font-bold text-start">Signup</h1>
-        <p className="text-start text-lg">
+        <h1 className="text-5xl font-bold text-start">Signup</h1>
+        <p className="text-start text-xl">
           Already have an account?{' '}
-          <Link to="/login" className="text-secondary font-bold underline">
+          <Link
+            to="/auth?redirect=login"
+            className="text-secondary font-bold underline"
+          >
             Login
           </Link>
         </p>
       </header>
       <div className="flex justify-between w-full gap-32">
         <div className="flex flex-col gap-4 w-full">
-          <label className="mt-8 text-lg text-primary/70">Full name</label>
+          <label className="mt-8 text-xl text-primary/70">Full name</label>
           <Input
             type="text"
             placeholder="Full name"
@@ -45,7 +49,7 @@ export default function SignupForm() {
           />
         </div>
         <div className="flex flex-col gap-4 w-full">
-          <label className="mt-8 text-lg text-primary/70">Email</label>
+          <label className="mt-8 text-xl text-primary/70">Email</label>
           <Input
             type="email"
             placeholder="person@example.com"
@@ -59,7 +63,7 @@ export default function SignupForm() {
       </div>
       <div className="flex justify-between w-full gap-32">
         <div className="flex flex-col gap-4 w-full">
-          <label className="mt-8 text-lg text-primary/70">Password</label>
+          <label className="mt-8 text-xl text-primary/70">Password</label>
           <Input
             type="password"
             placeholder="•••••••••"
@@ -71,7 +75,7 @@ export default function SignupForm() {
           />
         </div>
         <div className="flex flex-col gap-4 w-full">
-          <label className="mt-8 text-lg text-primary/70">
+          <label className="mt-8 text-xl text-primary/70">
             Confirm Password
           </label>
           <Input
@@ -87,7 +91,7 @@ export default function SignupForm() {
       </div>
       <div className="flex justify-between w-full gap-32">
         <div className="flex flex-col gap-4 w-[57%]">
-          <label className="mt-8 text-lg text-primary/70">Username</label>
+          <label className="mt-8 text-xl text-primary/70">Username</label>
           <Input
             type="text"
             placeholder="Username"
@@ -99,8 +103,8 @@ export default function SignupForm() {
           />
         </div>
       </div>
-      <div className="flex flex-col gap-4 w-full justify-start items-center">
-        <label className="mt-8 text-lg text-primary/70"> </label>
+      <div className="flex flex-col gap-4 w-full justify-start items-center -mt-12">
+        <label className="mt-8 text-xl text-primary/70"> </label>
         <div className="flex gap-8 mt-12 items-center justify-center">
           <Input
             type="checkbox"
@@ -111,10 +115,17 @@ export default function SignupForm() {
             name="termsOfPolicy"
             className="w-5 h-5 mt-2"
           />
-          <label className="text-lg text-primary/70">
+          <label className="text-xl text-primary/70">
             I have read an greed to the Terms of Sevice and Privacy Policy
           </label>
         </div>
+        <Button
+          text="Create Account"
+          onClick={() => {}}
+          variant="primary"
+          className="w-full mt-8"
+          type="submit"
+        />
       </div>
     </form>
   );
