2ab48f8ae1b176e6a880700a0acdafa0ddb582c4
santiagofv36 <59099144+santiagofv36@users.noreply.github.com>
2024-04-13 22:40:29 -0400

    fix(client): Layout fix

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -3,10 +3,12 @@ interface LayoutProps {
   sectionClassName?: string;
 }
 
-export default function Layout({ children }: LayoutProps) {
+export default function Layout({ children, sectionClassName }: LayoutProps) {
   return (
     <main className="flex flex-col justify-center items-center w-full lg:px-24">
-      <section className="flex flex-col lg:flex-row gap-12 justify-center p-5 mb-12 bg-bg-neutral w-full py-32">
+      <section
+        className={`flex flex-col lg:flex-row gap-12 justify-center p-5 mb-12 bg-bg-neutral w-full py-32 ${sectionClassName}`}
+      >
         {children}
       </section>
     </main>
