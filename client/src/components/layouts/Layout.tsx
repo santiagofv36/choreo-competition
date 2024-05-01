d81aff1972308408a4234c9124ba0aac98b22f8a
santiagofv36 <59099144+santiagofv36@users.noreply.github.com>
2024-05-01 03:33:18 -0400

    fix: bug with classname having undefined

--- client/src/components/layouts/Layout.tsx
+++ client/src/components/layouts/Layout.tsx
@@ -7,7 +7,9 @@ export default function Layout({ children, sectionClassName }: LayoutProps) {
   return (
     <main className="flex flex-col justify-center items-center w-full lg:px-24">
       <section
-        className={`flex flex-col lg:flex-row gap-12 justify-center p-5 mb-12 bg-bg-neutral w-full py-32 ${sectionClassName}`}
+        className={`flex flex-col lg:flex-row gap-12 justify-center p-5 mb-12 bg-bg-neutral w-full py-32 ${
+          sectionClassName ?? ''
+        }`}
       >
         {children}
       </section>
