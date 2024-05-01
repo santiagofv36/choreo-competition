62649041127c383f2b2d0283a9d70c564f10d84e
santiagofv36 <59099144+santiagofv36@users.noreply.github.com>
2024-05-01 02:26:00 -0400

    fix(api): Sending bad request if the values are below 0

--- api/repositories/product_repository.py
+++ api/repositories/product_repository.py
@@ -53,6 +53,14 @@ class ProductRepository:
         min_price: Optional[float] = None,
         max_price: Optional[float] = None,
     ):
+
+        if min_price is not None and max_price is not None:
+            if min_price < 0 or max_price < 0:
+                raise HTTPException(
+                    status_code=status.HTTP_400_BAD_REQUEST,
+                    detail="Price range must be positive",
+                )
+
         try:
             query = db.query(Product).options(
                 subqueryload(Product.images).load_only(ProductImage.image),
@@ -65,7 +73,7 @@ class ProductRepository:
                     Category.name.ilike(f"%{search}%"),
                 )
                 query = query.join(Category).filter(search_filter)
-            
+
             # Apply category_id filter
             if category_id:
                 query = query.filter(Product.category_id == category_id)
